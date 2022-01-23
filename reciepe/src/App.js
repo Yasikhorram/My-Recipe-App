30a4cb06f991c1b49cf9108b80f8717652fd008f
Yasikhorram <khorramyasaman@gmail.com>
2022-01-22 23:57:23 -0600

    add useEffect

--- reciepe/src/App.js
+++ reciepe/src/App.js
@@ -1,5 +1,30 @@
-function App() {
-  return <div className="App">HELLO</div>;
-}
+import { useEffect, useState } from "react";
+
+const App = () => {
+  const APP_ID = "6007d3b9";
+  const APP_KEY = "7ba8f81641d53bafda49b712b25c2738";
+  const req = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(req);
+    const data = await response.json();
+    console.log(data);
+  };
+
+  return (
+    <div className="App">
+      <form className="searchForm">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default App;
