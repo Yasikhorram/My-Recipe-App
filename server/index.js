72116daded788ed7aabfaec62aa1057ee6aa646b
Yasikhorram <khorramyasaman@gmail.com>
2022-02-02 23:50:00 -0600

    Add routing

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const port = 8000;
+const PORT = 9000;
 
 const express = require("express");
 const cors = require("cors");
@@ -6,4 +6,7 @@ const axios = require("axios");
 require("dotenv").config();
 const app = express();
 
-app.listen(8000, () => console.log("server is running on port 8000"));
+app.get("/", (req, res) => {
+  res.json("testing routing");
+});
+app.listen(8000, () => console.log("server is running on port 9000"));
