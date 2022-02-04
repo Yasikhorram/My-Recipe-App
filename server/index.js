c9c42cea9c56c199ffec34acff1ba9f9482f7c63
Yasikhorram <khorramyasaman@gmail.com>
2022-02-03 23:51:06 -0600

    testing route

--- server/index.js
+++ server/index.js
@@ -7,7 +7,14 @@ require("dotenv").config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("testing routing");
+//This enables json middleware on every route we create for our API
+app.use(express.json());
+
+app.use(cors());
+//test route
+app.get("/", (req, resp) => {
+  success: "Hello testing";
 });
+
+// listen on the port
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
