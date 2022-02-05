577f392cdc4b1e79636cd8d923aae057797fb13a
Yasikhorram <khorramyasaman@gmail.com>
2022-02-04 23:41:29 -0600

    express setup

--- server/index.js
+++ server/index.js
@@ -1,20 +1,29 @@
-const PORT = 9000;
+// const PORT = 9000;
+
+// const express = require("express");
+// const cors = require("cors");
+// const axios = require("axios");
+// require("dotenv").config();
+
+// const app = express();
+
+// //This enables json middleware on every route we create for our API
+// app.use(express.json());
+
+// app.use(cors());
+// //test route
+// app.get("/", (req, resp) => {
+//   success: "Hello testing";
+// });
+
+// // listen on the port
+// app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
 
 const express = require("express");
-const cors = require("cors");
-const axios = require("axios");
-require("dotenv").config();
-
 const app = express();
 
-//This enables json middleware on every route we create for our API
-app.use(express.json());
-
-app.use(cors());
-//test route
-app.get("/", (req, resp) => {
-  success: "Hello testing";
+app.get("/", function (req, res) {
+  res.send("Hello Hello");
 });
 
-// listen on the port
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
+app.listen(9000);
